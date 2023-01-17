92135badcb92716be3b689b43e20f7d956592dfd
name-q <1620206666@qq.com>
2023-01-17 11:15:08 +0800

    框架重构

--- src/components/button/index.js
+++
@@ -1,8 +0,0 @@
-import Taro from "@tarojs/taro"
-import { Button } from "@tarojs/components"
-
-export default class CvButton extends Taro.Component {
-  render(){
-    return <Button className='cv_button'>AtButton</Button>
-  }
-}
