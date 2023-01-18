af07febcfeb23042cd65c9ae93af53b28cdb6c43
name-q <1620206666@qq.com>
2023-01-18 19:57:58 +0800

    框架错误解决 & 测试构建

---
+++ src/components/test/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from '@tarojs/components'
+
+export default class CvTest extends React.Component<any, any> {
+  public render(): JSX.Element | null {
+    return (
+      <View>
+        <Text>test</Text>
+      </View>
+    )
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as CvTest } from './components/test'
